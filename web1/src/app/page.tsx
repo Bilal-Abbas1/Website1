4e2d1f9d983783c5a7044deef67dfa9adcf582ba
Bilal-Abbas1 <muhammedbilalabbas229@gmail.com>
2024-02-24 11:12:59 +0500

    first commit

---
+++ web1/src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Header from "@/components/header";
+import Hero from "@/components/Hero";
+import Hero2 from "@/components/Hero2";
+
+
+// import Navbar from "@/components/Navbar"
+export default function Home() {
+  return (
+   <main>
+{/* <Navbar /> */}
+    <Header />
+    
+    <Hero />
+   <Hero2 />
+
+    </main>
+   
+
+
+
+
+  );
+}
